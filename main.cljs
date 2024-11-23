1485320e3d775eb305c919e38274df0c1149da62
Chris McCormick <chris@mccormick.cx>
2024-11-23 12:45:34 +0000

    Mor slides.

--- main.cljs
+++ main.cljs
@@ -20,7 +20,9 @@
   [:<>
    [:style (str "section:nth-child("
                 (inc (mod (:slide @state)
-                          (aget (js/document.querySelectorAll "section") "length")))
+                          (aget
+                            (js/document.querySelectorAll "section")
+                            "length")))
                 ") { display: block; }")]
    [:main
     [:section
@@ -30,19 +32,55 @@
      [:h1 "Orange Site Question"]
      [:img {:src "hn.png"}]]
     [:section
-     [:h1 "Faster editing"]]
+     [:h1 "What's LISP?"]
+     [:h2
+      [:p [:code "."]]]]
     [:section
-     [:h1 "Less syntax to remember"]]
+     [:h1 "What is LISP?"]
+     [:h2
+      [:p [:code " print(42)"]]]]
     [:section
-     [:h1 "nREPL (fast iteration)"]]
+     [:h1 "What is LISP?"]
+     [:h2
+      [:p [:code "(print 42)"]]]]
     [:section
-     [:h1 "Metaprogramming"]]
+     [:h1 "What is LISP?"]
+     [:h2
+      [:p "Very simple to implement"]]]
+    [:section
+     [:h1 "What's the point of LISP?"]
+     [:h2
+      [:p "Four features"]]]
+    [:section
+     [:h1 "1. Faster editing & refactoring"]
+     [:h2
+      [:p "Your editor understands the language"]]]
+    [:section
+     [:h1 "2. Less syntax to remember"]
+     [:h2
+      [:p "Con: so many brackets!"]
+      [:p "Pro: so many brackets!"]]]
+    [:section
+     [:h1 "2. Less syntax to remember"]
+     [:h2
+      [:p [:code "q = 42"]]]]
+    [:section
+     [:h1 "2. Less syntax to remember"]
+     [:h2
+      [:p [:code "(def q 42)"]]]]
+    [:section
+     [:h1 "3. nREPL (fast iteration)"]
+     [:img {:src "cider-debug.gif"
+            :style {:width "80vw"}}]]
+    [:section
+     [:h1 "4. Metaprogramming"]
+     [:p "I am glad the big brained have access to this."]]
     [:section
      [:h1 "Try it"]
      [:h2
-     [:ul
-      [:li [:code "pip install basilisk"]]
-      [:li [:code "npm install nbb"]]]]]]])
+      [:ul
+       [:li [:code "pip install hy"]]
+       [:li [:code "npm install nbb"]]]]]]])
 
 (rdom/render [app] (.getElementById js/document "app"))
 (defonce keylistener (aset js/window "onkeydown" #(keydown %)))
